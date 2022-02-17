b62535297994fb2c60c9d35f4137e6009c6419a5
Woody <woodynadobhar@hotmail.com>
2022-02-17 14:53:21 -0800

    Added 'stupidpassword' validation rule for simpler implementation.
    Signed-off-by: Woody <woodynadobhar@hotmail.com>

--- config/laravelstupidpassword.php
+++ config/laravelstupidpassword.php
@@ -11,6 +11,9 @@ return [
     |
     */
 
+    //max pw length
+    'max' => env('PASSWORD_MAX', '40'),
+
     //local words to exclude, such as the site name, domain, etc
     'environmentals' => env('PASSWORD_ENVIRONMENTALS', []),
 
